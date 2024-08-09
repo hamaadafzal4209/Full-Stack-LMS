808c80591156baf3f73f7a50fc86afb2c1baa91d
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-09 03:48:31 -0700

    server setup and connected to database

--- LMS/server/server.ts
+++ LMS/server/server.ts
@@ -1,5 +1,40 @@
-import { app } from "./app";
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectDb } from "./utils/db";
+require("dotenv").config();
 
-app.listen(3000,() => {
-    console.log("Server is running")
-})
+const app = express();
+
+// body parser
+app.use(express.json({ limit: "50mb" }));
+
+// cookie parser
+app.use(cookieParser());
+
+// cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing api
+app.post("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: "API is working properly",
+  });
+});
+
+// unknown routes
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found!`) as any;
+  err.statusCode = 404;
+  next(err);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on localhost ${process.env.PORT}`);
+  connectDb();
+});
