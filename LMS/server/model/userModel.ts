4bf224fabae4a85191dfe03aba97393fa940b3f3
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-09 06:58:33 -0700

    commit

--- LMS/server/model/userModel.ts
+++ LMS/server/model/userModel.ts
@@ -27,7 +27,7 @@ const userSchema: Schema<IUser> = new mongoose.Schema(
       type: String,
       required: [true, "Please enter your email"],
       validate: {
-        validator: function (value: String) {
+        validator: function (value: string) {
           return emailRegexPattern.test(value);
         },
         message: "Please enter a valid email",
