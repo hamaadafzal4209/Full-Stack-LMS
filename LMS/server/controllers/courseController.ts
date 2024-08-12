d7c0347bfe3c3159ec234dbfe9f96d76ae3a429d
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-12 08:48:52 -0700

    commit

--- LMS/server/controllers/courseController.ts
+++ LMS/server/controllers/courseController.ts
@@ -1,4 +1,14 @@
+import { NextFunction, Request, Response } from "express";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 import courseModel from "../model/courseModel";
+import ErrorHandler from "../utils/ErrorHandler";
 
-export const uploadCourse = catchAsyncErrors(async)
+export const uploadCourse = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { data } = req.body;
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
