0a0db66c33b9e2230f0f763d49d4a6982b0372eb
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-13 04:41:57 -0700

    commit

--- LMS/server/controllers/courseController.ts
+++ LMS/server/controllers/courseController.ts
@@ -2,25 +2,25 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 import courseModel from "../model/courseModel";
 import ErrorHandler from "../utils/ErrorHandler";
-import cloudinary from 'cloudinary';
+import cloudinary from "cloudinary";
 import { createCourse } from "../services/courseService";
 
 export const uploadCourse = catchAsyncErrors(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const data = req.body;
-        const thumbnail = data.thumbnail;
-        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-          folder: "courses",
-        });
-  
-        data.thumbnail = {
-          public_id: myCloud.public_id,
-          url: myCloud.secure_url,
-        };
-  
-        createCourse(data, res, next);
-      } catch (error: any) {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+        folder: "courses",
+      });
+
+      data.thumbnail = {
+        public_id: myCloud.public_id,
+        url: myCloud.secure_url,
+      };
+
+      createCourse(data, res, next);
+    } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
     }
   }
