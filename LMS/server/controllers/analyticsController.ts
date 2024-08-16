5a28ad0071fd81ddb554f7d131d30aaaa9736203
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-16 03:40:02 -0700

    getCourseAnalytics

--- LMS/server/controllers/analyticsController.ts
+++ LMS/server/controllers/analyticsController.ts
@@ -4,6 +4,7 @@ import { generateLast12MonthsData } from "../utils/analyticsGenerator";
 import ErrorHandler from "../utils/ErrorHandler";
 import userModel from "../model/userModel";
 import orderModel from "../model/orderModel";
+import courseModel from "../model/courseModel";
 
 // Get users analytics -- admin
 export const getUserAnalytics = catchAsyncErrors(
@@ -21,13 +22,28 @@ export const getUserAnalytics = catchAsyncErrors(
 );
 
 // Get order analytics -- admin
-export const getAnalyticsOrders = catchAsyncErrors(
+export const getOrderAnalytics = catchAsyncErrors(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const orders = await generateLast12MonthsData(orderModel);
       res.status(200).json({
         success: true,
         orders: orders,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+// Get course analytics -- admin
+export const getCourseAnalytics = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const courses = await generateLast12MonthsData(courseModel);
+      res.status(200).json({
+        success: true,
+        courses: courses,
       });
     } catch (error: any) {
       return next(new ErrorHandler(error.message, 400));
