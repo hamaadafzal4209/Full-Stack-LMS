5af77c380c98bdfa4bb619bbe8d2a3c6ba660e0a
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-16 04:08:55 -0700

    commit

--- LMS/server/controllers/layoutController.ts
+++ LMS/server/controllers/layoutController.ts
@@ -2,73 +2,75 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 import layoutModel from "../model/layoutModel";
 import ErrorHandler from "../utils/ErrorHandler";
-import cloudinary from 'cloudinary';
+import cloudinary from "cloudinary";
 
 // create layout
-export const createLayout = catchAsyncErrors((async (req:Request,res:Response,next:NextFunction) => {
+export const createLayout = catchAsyncErrors(
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const { type } = req.body;
-        const isTypeExist = await layoutModel.findOne({ type });
-        if (isTypeExist) {
-          return next(new ErrorHandler(`Layout ${type} already exists`, 400));
-        }
-  
-        if (type === "Banner") {
-          const { image, title, subtitle } = req.body;
-          const result = await cloudinary.v2.uploader.upload(image, {
-            folder: "layout",
-          });
-  
-          const banner = {
-            type: "Banner",
-            banner: {
-              image: {
-                public_id: result.public_id,
-                url: result.secure_url,
-              },
-              title,
-              subtitle,
-            },
-          };
-  
-          await layoutModel.create(banner);
-        }
-  
-        if (type === "FAQ") {
-          const { faq } = req.body;
-          const faqItems = await Promise.all(
-            faq.map(async (item: any) => {
-              return {
-                question: item.question,
-                answer: item.answer,
-              };
-            })
-          );
-  
-          await layoutModel.create({ type: "FAQ", faq: faqItems });
-        }
-  
-        if (type === "Categories") {
-          const { categories } = req.body;
-          const categoriesItems = await Promise.all(
-            categories.map(async (item: any) => {
-              return {
-                title: item.title,
-              };
-            })
-          );
-  
-          await layoutModel.create({
-            type: "Categories",
-            categories: categoriesItems,
-          });
-        }
-  
-        res.status(200).json({
-          success: true,
-          message: "Layout created successfully",
-        });
-      } catch (error: any) {
-        return next(new ErrorHandler(error.message, 400));
+      const { type } = req.body;
+      const isTypeExist = await layoutModel.findOne({ type });
+      if (isTypeExist) {
+        return next(new ErrorHandler(`Layout ${type} already exists`, 400));
       }
-}))
+
+      if (type === "Banner") {
+        const { image, title, subtitle } = req.body;
+        const result = await cloudinary.v2.uploader.upload(image, {
+          folder: "layout",
+        });
+
+        const banner = {
+          type: "Banner",
+          banner: {
+            image: {
+              public_id: result.public_id,
+              url: result.secure_url,
+            },
+            title,
+            subtitle,
+          },
+        };
+
+        await layoutModel.create(banner);
+      }
+
+      if (type === "FAQ") {
+        const { faq } = req.body;
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+
+        await layoutModel.create({ type: "FAQ", faq: faqItems });
+      }
+
+      if (type === "Categories") {
+        const { categories } = req.body;
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+
+        await layoutModel.create({
+          type: "Categories",
+          categories: categoriesItems,
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Layout created successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
