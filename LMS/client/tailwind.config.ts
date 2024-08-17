3c1350b33f596c676145831fd3277dd8476ef709
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-16 23:04:58 -0700

    modify tailwind config file

--- LMS/client/tailwind.config.ts
+++ LMS/client/tailwind.config.ts
@@ -8,10 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
+        Cursive: ["var(--font-Cursive)"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
       },
     },
   },
