68cec4e2175bf567d6467f9bc0f5defe7de85414
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-19 06:19:03 -0700

    commit

--- LMS/client/app/Provider.tsx
+++ LMS/client/app/Provider.tsx
@@ -1,10 +1,8 @@
-import react, { ReactNode } from "react";
 import { Provider } from "react-redux";
-
 import { store } from "./redux/store";
 
 interface ProviderProps {
-  children: ReactNode;
+  children: any;
 }
 
 export function Providers({ children }: ProviderProps) {
