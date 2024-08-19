5aa1f220607113f8a8ab642d639c4d3052f3e75d
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-19 06:51:10 -0700

    create user registration RTK query

---
+++ LMS/client/app/redux/features/auth/authApi.ts
@@ -0,0 +1,41 @@
+import { apiSlice } from "../api/apiSlice";
+import { userRegistration } from "./authSlice";
+
+type RegistrationResponse = {
+  message: string;
+  activationToken: string;
+};
+
+type RegistrationData = {
+  // Define the expected structure of the registration data
+  username: string;
+  email: string;
+  password: string;
+};
+
+export const authApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    register: builder.mutation<RegistrationResponse, RegistrationData>({
+      query: (data) => ({
+        url: "registration",
+        method: "POST",
+        body: data,
+        credentials: "include",
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userRegistration({
+              token: result.data.activationToken,
+            })
+          );
+        } catch (error: any) {
+          console.error("Registration failed:", error);
+        }
+      },
+    }),
+  }),
+});
+
+export const { useRegisterMutation } = authApi;
