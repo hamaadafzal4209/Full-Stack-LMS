f2a6df43389c1345455c18176c7cf219aa02a338
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-18 21:25:11 -0700

    complete verification UI

--- LMS/client/app/components/Auth/Verification.tsx
+++ LMS/client/app/components/Auth/Verification.tsx
@@ -1,12 +1,98 @@
-import { styles } from '@/app/styles/style'
-import React from 'react'
+import { styles } from "@/app/styles/style";
+import React, { FC, useRef, useState } from "react";
+import { VscWorkspaceTrusted } from "react-icons/vsc";
 
-type Props = {}
+type Props = {
+  setRoute: (route: string) => void;
+};
 
-const Verification = (props: Props) => {
+type VerifyNumber = {
+  "0": string;
+  "1": string;
+  "2": string;
+  "3": string;
+};
+
+const Verification: FC<Props> = ({ setRoute = () => {} }) => {
+  const [invalidError, setInvalidError] = useState<boolean>(false);
+  const [verifyNumber, setVerifyNumber] = useState<VerifyNumber>({
+    0: "",
+    1: "",
+    2: "",
+    3: "",
+  });
+
+  const inputRefs = [
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+    useRef<HTMLInputElement>(null),
+  ];
+
+  const verifyHandler = async () => {
+    console.log("Verification handler triggered");
+  };
+
+  const handleInputChange = (index: number, value: string) => {
+    setInvalidError(false);
+    const newVerifyNumber = { ...verifyNumber, [index]: value };
+    setVerifyNumber(newVerifyNumber);
+
+    if (value === "" && index > 0) {
+      inputRefs[index - 1].current?.focus();
+    } else if (value.length === 1 && index < 3) {
+      inputRefs[index + 1].current?.focus();
+    }
+  };
+
   return (
-    <div className={`text-white ${styles.title}`}>Verification</div>
-  )
-}
+    <div>
+      <h1 className={`${styles.title}`}>Verify your account</h1>
+      <div className="w-full flex items-center justify-center my-2">
+        <div className="w-[80px] h-[80px] rounded-full bg-blue-600 flex items-center justify-center">
+          <VscWorkspaceTrusted
+            size={40}
+            className="text-white dark:text-black"
+          />
+        </div>
+      </div>
+      <div className="m-auto flex items-center justify-center gap-4 mt-6 mb-4">
+        {Object.keys(verifyNumber).map((key, index) => (
+          <input
+            type="number"
+            key={key}
+            ref={inputRefs[index]}
+            className={`w-16 h-16 bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${
+              invalidError
+                ? "shake border-red-500"
+                : "dark:border-white border-[#0000004a]"
+            }`}
+            placeholder=""
+            maxLength={1}
+            value={verifyNumber[key as keyof VerifyNumber]}
+            onChange={(e) => handleInputChange(index, e.target.value)}
+          />
+        ))}
+      </div>
+      <div className="w-full flex justify-center items-center">
+        <button
+          className={`max-w-80 w-full inline-flex justify-center whitespace-nowrap rounded-lg bg-indigo-500 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm shadow-indigo-950/10 hover:bg-indigo-600 focus:outline-none focus:ring focus:ring-indigo-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 transition-colors duration-150`}
+          onClick={verifyHandler}
+        >
+          Verify OTP
+        </button>
+      </div>
+      <h5 className="text-center pt-4 font-Poppins text-black dark:text-white text-[14px]">
+        Go back to Sign in?{" "}
+        <span
+          className="text-blue-600 pl-1 cursor-pointer underline"
+          onClick={() => setRoute("Login")}
+        >
+          Sign in
+        </span>
+      </h5>
+    </div>
+  );
+};
 
-export default Verification
+export default Verification;
