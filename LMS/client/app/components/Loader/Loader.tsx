f0a9aab411d709227940a878d357c517a3c247ee
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-20 08:44:15 -0700

    add loader and modify layout file

---
+++ LMS/client/app/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { ScatterBoxLoader } from "react-awesome-loaders";
+export const Loader = () => {
+  return (
+    <>
+      <ScatterBoxLoader
+        primaryColor={"#6366F1"}
+      />
+    </>
+  );
+};
+
--- LMS/client/app/layout.tsx
+++ LMS/client/app/layout.tsx
@@ -5,6 +5,9 @@ import { ThemeProvider } from "./utils/theme.provider";
 import { Toaster } from "react-hot-toast";
 import { Providers } from "./Provider";
 import { SessionProvider } from "next-auth/react";
+import { useLoadUserQuery } from "./redux/features/api/apiSlice";
+import { FC, ReactNode } from "react";
+import Loader from "./components/Loader/Loader";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -31,7 +34,7 @@ export default function RootLayout({
         <Providers>
           <SessionProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              {children}
+              <Custom>{children}</Custom>
               <Toaster position="top-center" reverseOrder={false} />
             </ThemeProvider>
           </SessionProvider>
@@ -40,3 +43,8 @@ export default function RootLayout({
     </html>
   );
 }
+
+const Custom: FC<{ children: ReactNode }> = ({ children }) => {
+  const { isLoading } = useLoadUserQuery({});
+  return <>{isLoading ? <Loader /> : children}</>;
+};
