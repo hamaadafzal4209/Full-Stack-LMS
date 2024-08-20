0d730fbdf9032a0b8ce55e79113a2f443aacfea8
FrontendFlair <hamaadafzal404@gmail.com>
2024-08-19 21:27:23 -0700

    commit

--- LMS/client/app/redux/features/api/apiSlice.ts
+++ LMS/client/app/redux/features/api/apiSlice.ts
@@ -7,7 +7,7 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     refreshToken: builder.query({
       query: () => ({
-        url: "refresh-token",
+        url: "refresh",
         method: "GET",
         credentials: "include" as const,
       }),
